1ebe871f8e427221866f933d7a655256629c3e1b
disconnect-ed <ruslan199812@gmail.com>
2020-09-22 20:07:18 +0300

    Updates

---
+++ precache-manifest.ad1dbf2c3a4831b5bc351ff350b87b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf082d7bbb2866c866b9b1e814d9982",
+    "url": "/2D-grid/index.html"
+  },
+  {
+    "revision": "6d853869fccbd5465d22",
+    "url": "/2D-grid/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "8c362c00a73ced6b6e45",
+    "url": "/2D-grid/static/css/main.fa123b74.chunk.css"
+  },
+  {
+    "revision": "6d853869fccbd5465d22",
+    "url": "/2D-grid/static/js/2.b3d4e5ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/2D-grid/static/js/2.b3d4e5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c362c00a73ced6b6e45",
+    "url": "/2D-grid/static/js/main.fbf94803.chunk.js"
+  },
+  {
+    "revision": "19ebc0d560a88833f517",
+    "url": "/2D-grid/static/js/runtime-main.68ff3fc0.js"
+  }
+]);
