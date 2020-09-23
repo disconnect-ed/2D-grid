c7a75ae60bb9987a597251a3393ca56224b9210f
disconnect-ed <ruslan199812@gmail.com>
2020-09-23 09:17:11 +0300

    Updates

--- precache-manifest.5d9608403fc25e96c3b33637343fa9e6.js
+++ precache-manifest.5d9608403fc25e96c3b33637343fa9e6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd5a644ef63d0082a681e302922d143c",
+    "revision": "65cb3458bac72aab79d69b07f039e819",
     "url": "/2D-grid/index.html"
   },
   {
-    "revision": "6d853869fccbd5465d22",
+    "revision": "7ec5de5918018c4fd802",
     "url": "/2D-grid/static/css/2.99a5358a.chunk.css"
   },
   {
-    "revision": "16ef6c0a0dcbd1bb5355",
+    "revision": "4945d535c5e995906def",
     "url": "/2D-grid/static/css/main.c942cb93.chunk.css"
   },
   {
-    "revision": "6d853869fccbd5465d22",
-    "url": "/2D-grid/static/js/2.b3d4e5ed.chunk.js"
+    "revision": "7ec5de5918018c4fd802",
+    "url": "/2D-grid/static/js/2.43be02f2.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/2D-grid/static/js/2.b3d4e5ed.chunk.js.LICENSE.txt"
+    "url": "/2D-grid/static/js/2.43be02f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "16ef6c0a0dcbd1bb5355",
-    "url": "/2D-grid/static/js/main.4199caad.chunk.js"
+    "revision": "4945d535c5e995906def",
+    "url": "/2D-grid/static/js/main.14db1841.chunk.js"
   },
   {
     "revision": "19ebc0d560a88833f517",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2D-grid/precache-manifest.e12215dae0b1c988c4f7c5b96cc3df16.js"
+  "/2D-grid/precache-manifest.5d9608403fc25e96c3b33637343fa9e6.js"
 );
 
 self.addEventListener('message', (event) => {
